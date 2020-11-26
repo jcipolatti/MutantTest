7687b51551e78f62a100a239b86d3474716c48e0
jcipolatti <julian.cipolatti@cablasociados.com>
2020-11-26 11:38:20 -0300

    adding swagger

--- src/main/java/com/magneto/mutanttest/MutantTestApplication.java
+++ src/main/java/com/magneto/mutanttest/MutantTestApplication.java
@@ -1,13 +1,23 @@
 package com.magneto.mutanttest;
 
+import com.magneto.mutanttest.configuration.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class MutantTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MutantTestApplication.class, args);
     }
 
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+    }
 }
